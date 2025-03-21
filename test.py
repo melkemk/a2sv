7a526e0281838f5153c7884c7dc32ee77c0a4751
melkamu <melkemk503@gmail.com>
2025-03-21 18:35:57 +0300

    test

---
+++ test.py
@@ -0,0 +1,32 @@
+import unittest
+from io import StringIO
+import sys
+from main import fizz_buzz
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_fizz_buzz(self):
+        output = StringIO()
+        sys.stdout = output
+        fizz_buzz(15)
+        sys.stdout = sys.__stdout__
+        expected_output = (
+            "1\n"
+            "2\n"
+            "Fizz\n"
+            "4\n"
+            "Buzz\n"
+            "Fizz\n"
+            "7\n"
+            "8\n"
+            "Fizz\n"
+            "Buzz\n"
+            "11\n"
+            "Fizz\n"
+            "13\n"
+            "14\n"
+            "FizzBuzz\n"
+        )
+        self.assertEqual(output.getvalue(), expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
